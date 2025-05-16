48c86ff95f2dd5ec434d30f5ada57b200694cfaa
Mohamed Saeed <muhameds913@gmail.com>
2025-05-16 23:55:07 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿
 using ConsoleApp2.Data;
 using ConsoleApp2.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Numerics;
 
 
 namespace ConsoleApp2
@@ -27,6 +29,44 @@ namespace ConsoleApp2
             //    Console.WriteLine(Order.OrderId);
             //}
 
+
+            //Get all products that belong to a category named "Mountain Bikes"
+            //var products = _context.Products.Include(p => p.Category).Where(p=> p.Category.CategoryName == "Mountain Bikes").ToList();    
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine(product.ProductName); 
+            //}
+
+            //Count the total number of orders per store.
+
+            //var totalNumOfStores = _context.Orders.Include(s => s.Store).GroupBy(s => s.Store.StoreName).Select(s => new {s.Key , OrderCount = s.Count() }).ToList();
+            //foreach (var store in totalNumOfStores)
+            //{
+            //    Console.WriteLine($" StoreName = {store.Key}, Count = {store.OrderCount}");
+            //}
+
+
+            //List all orders that have not been shipped yet(shipped_date is null)
+
+            //var NotShippedOrders = _context.Orders.Where(o => o.ShippedDate == null);
+            //foreach (var orders in NotShippedOrders)
+            //{
+            //    Console.WriteLine($"OrderId = {orders.OrderId}");
+            //}
+
+            //Display each customer’s full name and the number of orders they have placed
+
+            //var customerOrderCounts = _context.Customers.Include(o => o.Orders).GroupBy(x => new { x.FirstName, x.LastName }).Select(g => new {FullName = g.Key.FirstName + " " + g.Key.LastName,OrderCount = g.Count()});
+            //var x = 0;
+            //foreach (var orders in customerOrderCounts)
+            //{
+            //    Console.WriteLine($"FullName = {orders.FullName} {orders.OrderCount} ");
+            //}
+
+            //List all products that have never been ordered(not found in order_items)
+            //??
+
+            //Display products that have a quantity of less than 5 in any store stock
 
         }
     }
